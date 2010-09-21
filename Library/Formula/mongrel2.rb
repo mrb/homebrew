02164520c024e08db02eebe44f2785292ef92cb1
mrb <michaelrbernstein@gmail.com>
2010-09-21 06:26:01 -0400

    mongrel2 formula added

---
+++ Library/Formula/mongrel2.rb
@@ -0,0 +1,15 @@
+require 'formula'
+
+class Mongrel2 <Formula
+  url 'http://mongrel2.org/static/downloads/mongrel2-1.1.tar.bz2'
+  homepage 'http://mongrel2.org/'
+  md5 '62ae9294840d7c5e026c70c9efd40324'
+  
+  depends_on 'zeromq'
+  depends_on 'sqlite'
+
+  def install
+    system "make all"
+    system "make install PREFIX=#{prefix}"
+  end
+end
